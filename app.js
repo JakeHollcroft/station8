e1012ec4b7bccb29c55ab65b44039dccdcc764bc
Jake <jakehollcroft918@gmail.com>
2021-04-24 13:36:57 -0500

    add start script

--- app.js
+++ app.js
@@ -67,6 +67,8 @@ app.delete('/profiles/:id', async (req, res) => {
     res.redirect('/profiles');
 })
 
-app.listen(3000, () => [
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => [
+    console.log(`Serving on port ${port}`)
 ])
